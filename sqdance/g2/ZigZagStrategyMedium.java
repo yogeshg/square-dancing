da88fdb2505d222a04a26ee8e642577ed1f7204a
Rajan Bhargava <rajanbhargava2002@gmail.com>
2016-11-15 17:02:17 -0500

    New simulator, strategy interface and small, medium d strategy classes

---
+++ sqdance/g2/ZigZagStrategyMedium.java
@@ -0,0 +1,16 @@
+package sqdance.g2;
+
+import sqdance.sim.Point;
+
+public class ZigZagStrategyMedium implements Strategy {
+	
+	@Override
+	public Point[] generate_starting_locations() {
+		return new ZigZagStrategySmall().generate_starting_locations();
+	}
+
+	@Override
+	public Point[] play(Point[] dancers, int[] scores, int[] partner_ids, int[] enjoyment_gained) {
+		return null;
+	}
+}
